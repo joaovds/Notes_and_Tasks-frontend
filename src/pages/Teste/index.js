ed1535d9f77a38d6b238d2d5b2d5466d234d3f88
joaovds <jv782063@gmail.com>
2020-05-01 17:45:13 -0300

    testing and creating the base of the routes

---
+++ src/pages/Teste/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './style.css';
+
+export default function Teste() {
+    return (
+        <div className="teste-container">
+            <h1>Testing!!!...</h1>
+        </div>
+    );
+}
