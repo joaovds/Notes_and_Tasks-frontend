0779dbe8acfb1fa9377f35d5b7a15a79bc0ce1e6
joaovds <jv782063@gmail.com>
2020-05-17 17:42:39 -0300

    page register, front

---
+++ src/pages/Register/index.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Footer from '../FooterPages';
+import './style.css';
+
+import logoImg from '../../assets/logo.png';
+import tasksBan2 from '../../assets/undraw_tasks2.png';
+
+export default function Register() {
+    return (
+        <div className="register-container">
+            <header>
+                <div className="logo">
+                    <img src={ logoImg } alt="Logo" className="logo-header" />
+                </div>
+                
+                <div className="buttons">
+                    <Link className="button login" to="/login">
+                        Log in
+                    </Link>
+                    <Link className="button register" to="/register">
+                        Register
+                    </Link>
+                </div>
+            </header>
+
+            <section>
+                <img src={ tasksBan2 } alt="tasks" className="tasksBan2" />
+                <form>
+                    <div className="title">
+                        <h1>Register</h1>
+                    </div>
+
+                    <div className="inputs">
+                        <label for="name">
+                            Nome
+                        </label>
+                        <input
+                            name="nome"
+                            type="text"
+                        />
+                        <label for="email">
+                            E-mail
+                        </label>
+                        <input
+                            name="email"
+                            type="email"
+                        />
+                        <label for="password">
+                            Password
+                        </label>
+                        <input
+                            name="password"
+                            type="password"
+                        />
+
+                        <button className="button" type="submit">Criar</button>
+                        <Link className="button createacont" to="/">
+                            Voltar
+                        </Link>
+                    </div>
+                </form>
+            </section>
+
+            <Footer />
+        </div>
+    );
+}
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import InitialPage from './pages/InitialPage';
 import Login from './pages/Login';
+import Register from './pages/Register';
 
 export default function Routes() {
     return (
@@ -10,6 +11,7 @@ export default function Routes() {
             <Switch>
                 <Route path="/" exact component={InitialPage} />
                 <Route path="/login" component={Login} />
+                <Route path="/register" component={Register} />
             </Switch>
         </BrowserRouter>
     );
